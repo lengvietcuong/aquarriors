bfb6efc16ba7b5955302f19805cdc6c4ffe1d918
lengvietcuong <lengvietcuong@gmail.com>
2024-06-29 14:27:30 +0700

    add goal counter

---
+++ aquarriors-website/components/hero-section/GoalCounter.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import CountUp from "react-countup";
+
+function GoalCounter() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, []);
+
+  return (
+    <h2 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-primary to-foreground text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-center">
+      <CountUp end={10_000_000} duration={1.5} />
+    </h2>
+  );
+}
+
+export default GoalCounter;
