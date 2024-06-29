fc129b136486c11c6f3eec49b83d97925a68b301
lengvietcuong <lengvietcuong@gmail.com>
2024-06-29 15:38:04 +0700

    fix text border issue

--- aquarriors-website/components/hero-section/GoalCounter.tsx
+++ aquarriors-website/components/hero-section/GoalCounter.tsx
@@ -11,7 +11,7 @@ function GoalCounter() {
   }, []);
 
   return (
-    <h2 className="font-extrabold text-transparent bg-clip-text bg-gradient-to-b from-primary to-foreground text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-center">
+    <h2 className={`font-extrabold ${isLoading ? "" : "text-transparent bg-clip-text bg-gradient-to-b from-primary to-foreground"} text-5xl sm:text-7xl md:text-8xl lg:text-9xl text-center`}>
       {isLoading ? (
         <span className="animation-pulse text-muted">Loading...</span>
       ) : (
